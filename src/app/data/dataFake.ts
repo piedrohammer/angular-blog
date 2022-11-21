35d21cd093ab45287b1e1164f40964873fc02dc8
piedrohammer <piiedrohammer@gmail.com>
2022-11-21 12:10:59 -0300

    final commit

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "NOVO HOMEM DE FERRO EM 3D",
+    "description": "Marvel anuncia um novo filme do homem de ferro, confira",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    "id":"2",
+    "title": "Nova Série anunciada no Disney +",
+    "description": "Marvel anuncia novas série para a plataforma de Streaming Disney +",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+  {
+    "id":"3",
+    "title": "Pantera Negra: Wakanda Para Sempre",
+    "description": "Lançamento do filme Pantera Negra: Wakanda Para Sempre lançado no dia 10 de novembro de 2022, Pantera Negra 2 foi um sucesso! ",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2022/10/Pantera-Negra-2-Shuri.jpg"
+  },
+  {
+    "id":"4",
+    "title": "Novo filme dos X-men está em pós produção, afirma diretor",
+    "description": "Diretores afirmam que novo filme dos X-Mens está em pós produção com lançamento planejado para segundo semestre de 2024 ",
+    "photoCover":"https://midias.correiobraziliense.com.br/_midias/jpg/2022/07/22/675x450/1_xmen-26096415.jpeg?20220722171339?20220722171339"
+  }
+]
